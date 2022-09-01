ff0e9641fee336c9c849f9c15fb81bd8e8071cb3
Quang Huy Ha Duyen <haduyenquanghuy@gmail.com>
2022-09-01 21:58:55 +0700

    fix some bugs

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -33,7 +33,7 @@ class ResourceParser(Parser):
     extensions = [".jpg", ".png", ".gif", ".css", ".html"]
 
     def parse(self, path: Path, source: Path, dest: Path):
-        self.copy(self, path, source, dest)
+        self.copy(path, source, dest)
 
 
 class MarkdownParser(Parser):
@@ -56,3 +56,4 @@ class ReStructuredTextParser(Parser):
         html = publish_parts(content.body, writer_name="html5")
         self.write(path, dest, html["html_body"])
         sys.stdout.write("\x1b[1;32m{} converted to HTML. Metadata: {}\n".format(path.name, content))
+        
